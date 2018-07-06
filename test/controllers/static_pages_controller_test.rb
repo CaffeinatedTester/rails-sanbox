84d363344417adf8aa4662b40456a10086928ae5
David Skinner <David.Skinner@missguided.com>
2018-07-06 13:44:26 +0100

    dry up code with application helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,36 +3,30 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "| Caffienated Tester Sandbox"
-  end
-
-  test "should get home when visiting root" do
-    get root_url
-    assert_response :success
-    assert_select 'title', "Home #{@base_title}"
+    @base_title = "Caffeinated Tester\'s Sandbox"
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
-    assert_select 'title', "Home #{@base_title}"
+    assert_select 'title', @base_title
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', "Help #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "About #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', "Contact #{@base_title}"
+    assert_select 'title', "Contact | #{@base_title}"
   end
 end
